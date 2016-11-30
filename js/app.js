aabb7d084797dae656d524e6f1ce2117d01cb9dc
Łuksz Strachanowski <lukasz.strachanowski@gmail.com>
2016-11-30 23:08:12 +0100

    deploy'

---
+++ js/app.js
@@ -0,0 +1,51 @@
+(function(){
+'use strict';
+
+var app = angular.module('MyApp',[]);
+
+app.controller('LunchCheckController',LunchCheckController);
+LunchCheckController.$inject = ['$scope'];
+
+function LunchCheckController($scope){
+	$scope.inputText ="";
+	$scope.displayMessage = function() {
+		var txt = $scope.inputText;
+		var meal = txt.split(',');
+		cleanArray(meal);
+		console.log(meal.length);
+		if(txt.length == 0){
+			$scope.inputVal ="Please enter data first";			
+			changeColor(false);
+		}else if (meal.length <= 3 && meal.length > 0) {
+			$scope.inputVal ="Enjoy";		
+			changeColor(true);
+		}else{
+			$scope.inputVal ="Too much!";
+			changeColor(true);
+		}
+	}
+}
+function cleanArray(arrayValue){
+	for (var i = 0 ; i < arrayValue.length ; i++) {
+		if (arrayValue[i] =="" || arrayValue[i] ==" "){
+			arrayValue.splice(i,1);
+		}
+	}
+}
+
+function changeColor(boolean){
+	var y = document.getElementsByClassName('form-control');
+	var x = document.getElementsByClassName('message');
+		if(boolean === true)
+			{
+				y[0].style.border = "1px solid green";
+				x[0].style.color = "green";
+			}else{
+				y[0].style.border = "1px solid red";
+				x[0].style.color = "red";
+			}	
+}
+})();
+
+
+
